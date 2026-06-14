f8cdf3d3450e67831f41712c478e3260395fc2c1
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-06-14 13:08:04 +0200

    Fix typing issue

--- api/tool/tag2link.py
+++ api/tool/tag2link.py
@@ -1,13 +1,13 @@
 # https://raw.githubusercontent.com/JOSM/tag2link/master/index.json
 
 import json
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 
 class tag2link:
     def __init__(self, rulesFiles: str):
         rules_json = json.load(open(rulesFiles, "r", encoding="utf-8"))
-        self.rules: Dict[(str, str), str] = {}
+        self.rules: Dict[Tuple[str, str], str] = {}
         for rule in rules_json:
             key = rule["key"]
             if not key.startswith("Key:"):
